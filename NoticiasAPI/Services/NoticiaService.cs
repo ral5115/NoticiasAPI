d1b84a8aa8ca8df172ea8cc056ae3f8c497df9ce
andres lopez <darkandres22@gmail.com>
2019-09-09 17:25:10 -0500

    agrega servicio para agregar noticia

---
+++ NoticiasAPI/Services/NoticiaService.cs
@@ -0,0 +1,33 @@
+﻿using NoticiasAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasAPI.Services
+{
+    public class NoticiaService
+    {
+        private readonly NoticiasDBContext dBContext;
+        public NoticiaService(NoticiasDBContext DBContext)
+        {
+            dBContext = DBContext;
+        }
+
+        public Boolean AgregarNoticia(Noticias noticia)
+        {
+            try
+            {
+                dBContext.Noticia.Add(noticia);
+                dBContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+           
+        }
+    }
+}
